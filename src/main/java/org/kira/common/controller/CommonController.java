417348e3e88abe488e48b0b5039dc703408a168c
zhangchaoqing <zzccqq2016@126.com>
2022-11-02 18:16:12 +0800

    commit

--- src/main/java/org/kira/common/controller/CommonController.java
+++ src/main/java/org/kira/common/controller/CommonController.java
@@ -1,20 +1,20 @@
-package org.kira.common.controller;
-
-import org.kira.common.resp.CommonResp;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author zhang chaoqing
- * Create on 2022/7/27 AT 0:11
- */
-@RestController
-public class CommonController {
-
-    @GetMapping("test")
-    public CommonResp<?> common() {
-        return CommonResp.success();
-    }
-
-
-}
+//package org.kira.common.controller;
+//
+//import org.kira.common.resp.CommonResp;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @author zhang chaoqing
+// * Create on 2022/7/27 AT 0:11
+// */
+//@RestController
+//public class CommonController {
+//
+//    @GetMapping("test")
+//    public CommonResp<?> common() {
+//        return CommonResp.success();
+//    }
+//
+//
+//}
